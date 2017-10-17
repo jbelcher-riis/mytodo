a8ba1633a4830ed31fe7d41d84bd245eca8052ea
Jim Belcher <jbelcher@riis.com>
2017-10-17 12:11:52 -0400

    initial commit, setup spring boot

---
+++ src/main/java/com/codecorner/mytodo/mytodo/MytodoApplication.java
@@ -0,0 +1,12 @@
+package com.codecorner.mytodo.mytodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MytodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MytodoApplication.class, args);
+	}
+}
---
+++ src/test/java/com/codecorner/mytodo/mytodo/MytodoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.codecorner.mytodo.mytodo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MytodoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
